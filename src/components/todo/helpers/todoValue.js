3cc14e495edd148e73dd482b310000ec423b6f18
Ihor <ihorzaliskyi@gmail.com>
2018-03-07 17:09:05 +0200

    validation input

--- src/components/todo/helpers/todoValue.js
+++ src/components/todo/helpers/todoValue.js
@@ -1,14 +1,4 @@
-const hendleValue = (value) => {
-  const valInput = Array.of(value)
-    .map(todo => todo.trim())
-    .filter(todo => {
-       const reg = /q|w|e|r|t|y|u|i|o|p|a|s|d|g|h|j|k|l|z|x|c|v|b|n|m|1|2|3|4|5|6|7|8|9|0/i
-        const todos = reg.test(todo)  
-         return todos
-       } )
- return valInput
-}
+const hendleValue = (value) => Array.of(value)
+  .filter(todo => todo.match(/^[\d\wа-я]+$/gi))
 
-export {
-  hendleValue
-}
+export {hendleValue}
