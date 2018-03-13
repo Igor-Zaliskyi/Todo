594e5ad9ecef48c00a8fc9b4fc7399c805339b61
Yurii Levantovych <domovoj1@gmail.com>
2018-03-13 18:55:05 +0200

    Reverted formatting

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
-import TodoCounts from './TodoCounts';
-import TodoFormAdd from './TodoFormAdd';
-import TodoItem from './TodoItem';
-import { addTodo, removeTodo, updateTodo } from 'api';
-import { isValidValue } from './helpers';
+import React, { Component } from 'react'
+import TodoCounts from './TodoCounts'
+import TodoFormAdd from './TodoFormAdd'
+import TodoItem from './TodoItem'
+import { addTodo, removeTodo, updateTodo } from 'api'
+import { isValidValue } from './helpers'
 
 export class Todo extends Component {
     constructor(props) {
-        super(props);
-        this.handleAddTodo = this.handleAddTodo.bind(this);
-        this.handleUpdateStatus = this.handleUpdateStatus.bind(this);
-        this.handleUpdateTitle = this.handleUpdateTitle.bind(this);
-        this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
+        super(props)
+        this.handleAddTodo = this.handleAddTodo.bind(this)
+        this.handleUpdateStatus = this.handleUpdateStatus.bind(this)
+        this.handleUpdateTitle = this.handleUpdateTitle.bind(this)
+        this.handleRemoveTodo = this.handleRemoveTodo.bind(this)
     }
 
     handleUpdateStatus(id, isChecked) {
         return this.onUpdateTodo({
             id,
             isChecked
-        });
+        })
     }
 
     handleUpdateTitle(id, value) {
         return this.onUpdateTodo({
             id,
             value
-        });
+        })
     }
 
     handleRemoveTodo(todoId) {
         return removeTodo(todoId)
-            .then(this.props.onFetchTodos);
+            .then(this.props.onFetchTodos)
     }
 
-    handleAddTodo(event, value) {
-        event.preventDefault();
-        const trimmedValue = value.trim();
+    handleAddTodo(event, value ) {
+        event.preventDefault()
+        const trimmedValue = value.trim()
         if (isValidValue(trimmedValue)) {
-            return addTodo({ value: trimmedValue })
-                .then(this.props.onFetchTodos);
+           return  addTodo({ value: trimmedValue })
+           .then(this.props.onFetchTodos)
         }
     }
 
     onUpdateTodo(todo) {
         return updateTodo(todo)
-            .then(this.props.onFetchTodos);
+            .then(this.props.onFetchTodos)
     }
 
     render() {
-        const { todos } = this.props;
+        const { todos } = this.props
         return (
             <div>
                 <TodoCounts todos={todos} />
@@ -82,6 +82,6 @@ export class Todo extends Component {
                     ))}
                 </ul>
             </div>
-        );
+        )
     }
 }
