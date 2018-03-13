fce54861db83c1aaebb8b3b7e6fc8993fa416792
Yurii Levantovych <domovoj1@gmail.com>
2018-03-13 18:52:40 +0200

    Added layout for status filters

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,60 +1,74 @@
-import React, { Component } from 'react'
-import TodoCounts from './TodoCounts'
-import TodoFormAdd from './TodoFormAdd'
-import TodoItem from './TodoItem'
-import { addTodo, removeTodo, updateTodo } from 'api'
-import { isValidValue } from './helpers'
+import React, { Component } from 'react';
+import TodoCounts from './TodoCounts';
+import TodoFormAdd from './TodoFormAdd';
+import TodoItem from './TodoItem';
+import { addTodo, removeTodo, updateTodo } from 'api';
+import { isValidValue } from './helpers';
 
 export class Todo extends Component {
     constructor(props) {
-        super(props)
-        this.handleAddTodo = this.handleAddTodo.bind(this)
-        this.handleUpdateStatus = this.handleUpdateStatus.bind(this)
-        this.handleUpdateTitle = this.handleUpdateTitle.bind(this)
-        this.handleRemoveTodo = this.handleRemoveTodo.bind(this)
+        super(props);
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+        this.handleUpdateStatus = this.handleUpdateStatus.bind(this);
+        this.handleUpdateTitle = this.handleUpdateTitle.bind(this);
+        this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
     }
 
     handleUpdateStatus(id, isChecked) {
         return this.onUpdateTodo({
             id,
             isChecked
-        })
+        });
     }
 
     handleUpdateTitle(id, value) {
         return this.onUpdateTodo({
             id,
             value
-        })
+        });
     }
 
     handleRemoveTodo(todoId) {
         return removeTodo(todoId)
-            .then(this.props.onFetchTodos)
+            .then(this.props.onFetchTodos);
     }
 
-    handleAddTodo(event, value ) {
-        event.preventDefault()
-        const trimmedValue = value.trim()
+    handleAddTodo(event, value) {
+        event.preventDefault();
+        const trimmedValue = value.trim();
         if (isValidValue(trimmedValue)) {
-           return  addTodo({ value: trimmedValue })          
-           .then(this.props.onFetchTodos) 
+            return addTodo({ value: trimmedValue })
+                .then(this.props.onFetchTodos);
         }
     }
 
     onUpdateTodo(todo) {
         return updateTodo(todo)
-            .then(this.props.onFetchTodos)
+            .then(this.props.onFetchTodos);
     }
 
     render() {
-        const { todos } = this.props
+        const { todos } = this.props;
         return (
             <div>
                 <TodoCounts todos={todos} />
                 <TodoFormAdd
                     onAddTodo={this.handleAddTodo}
                 />
+                <div class="wrapper-filters">
+                    <b>Filters:</b>
+                    <ul class="list-status-filters">
+                        <li>
+                            <button class="active">All</button>
+                        </li>
+                        <li>
+                            <button>Done</button>
+                        </li>
+                        <li>
+                            <button>Undone</button>
+                        </li>
+                    </ul>
+                </div>
                 <ul className="todo-list">
                     {todos.map(todo => (
                         <TodoItem
@@ -63,11 +77,11 @@ export class Todo extends Component {
                             onUpdateStatus={this.handleUpdateStatus}
                             onUpdateTitle={this.handleUpdateTitle}
                             onRemoveTodo={this.handleRemoveTodo}
-                           
+
                         />
                     ))}
                 </ul>
             </div>
-        )
+        );
     }
 }
